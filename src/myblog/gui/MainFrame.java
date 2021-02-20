7963450ae4a9a737b61ad7dbd38fe3a8b40dc67b
chenxiaopang <chenbin512@126.com>
2021-02-21 00:08:42 +0800

    add gui frame

---
+++ src/myblog/gui/MainFrame.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package myblog.gui;
+
+import javax.swing.JFrame;
+
+/**
+ * @author cb
+ *
+ */
+public class MainFrame extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame frame=new MainFrame();
+		frame.setSize(400, 300);
+		frame.setVisible(true);
+	}
+
+}
